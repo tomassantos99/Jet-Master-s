614f101b01ac1bc7646f881930fdcc5313d93d20
Tomás Santos <75134545+tomassantos99@users.noreply.github.com>
2022-04-04 21:08:02 +0100

    Added main menu and game over screen.

---
+++ Jetpack Masters/Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public string gameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
